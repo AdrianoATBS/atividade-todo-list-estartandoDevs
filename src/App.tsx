f0172e5bbb3501c14f032bb492cef52063a74f18
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-10-05 15:28:56 -0300

    feat(App.tsx): Adiciona função de remover item e função de concluir tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,11 @@ function App() {
   const [tarefa, setTarefa] = useState<Tarefa[]>([])
   const [inputValue, setInputValue] = useState('')
 
-  function handlerAdicionaTarefa(tarefa: string) {
+
+  function handlerAdicionaTarefa(inputValue: string) {
     const novaTarefa = {
       id: uuidv4(),
-      tarefa: tarefa,
+      tarefa: inputValue,
       isCompleted: false
     }
     setTarefa([...tarefa, novaTarefa])
@@ -28,6 +29,21 @@ function App() {
   {
     setInputValue(event.target.value)
   }
+  function handlerDeleteTarefa(id: string)
+  {
+    const tarefasAtualizadas = tarefa.filter(tarefa => tarefa.id !== id)
+    setTarefa(tarefasAtualizadas)
+  }
+  function handlerConcluirTarefa(id: string)
+  {
+    const tarefasAtualizadas = tarefa.map(tarefa => {
+      if (tarefa.id === id) {
+        return { ...tarefa, isCompleted: !tarefa.isCompleted };
+      }
+      return tarefa;
+    });
+    setTarefa(tarefasAtualizadas);
+  }
 
 
   return (
@@ -36,6 +52,8 @@ function App() {
         handlerAdicionaTarefa={handlerAdicionaTarefa}
         handlerInputChange={handlerInputChange} 
         inputValue={inputValue}
+        handlerDeleteTask={handlerDeleteTarefa}
+        handlerConcluirTask={handlerConcluirTarefa}
         />
     </>
   )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,10 @@
-export default function TodoList({ tarefa, handlerAdicionaTarefa, handlerInputChange, inputValue }) {
+export default function TodoList({ 
+    tarefa, 
+    handlerAdicionaTarefa, 
+    handlerInputChange, 
+    inputValue, 
+    handlerDeleteTask, 
+    handlerConcluirTask }) {
 
     return(
         <div className="TodoList">
@@ -11,9 +17,9 @@ export default function TodoList({ tarefa, handlerAdicionaTarefa, handlerInputCh
             <div className="tasks-list">
                 {tarefa.map((usuario) => (
                     <div key={usuario.id} className="task-item">
-                        <input type="checkbox" checked={usuario.isCompleted} />
+                        <input onClick={() => handlerConcluirTask(usuario.id)} type="checkbox" checked={usuario.isCompleted} />
                         <span className={usuario.isCompleted ? 'Concluida' : ''}>{usuario.tarefa}</span>
-                        <button className="bota-excluir">Excluir</button>
+                        <button onClick={() => handlerDeleteTask(usuario.id)} className="bota-excluir">Excluir</button>
                     </div>
                 ))}
             </div>
